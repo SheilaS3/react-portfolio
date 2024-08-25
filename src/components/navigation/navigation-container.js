7370f0decaffb7be989b465dff2adb44a3546288
SheilaS3 <sheilaportu@hotmail.com>
2024-08-25 20:48:21 +0200

    Logout Functionality with a Higher Order Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import { withRouter } from 'react-router';
+
 import { NavLink } from 'react-router-dom';
 
 const NavigationContainer = (props) => {
@@ -10,6 +13,21 @@ const NavigationContainer = (props) => {
                 </NavLink>
             </div>
         );
+    };
+
+    const handleSignOut = () => {
+        axios
+        .delete("https://api.devcamp.space/logout", { withCredentials: true })
+        .then(response => {
+            if (response.status === 200) {
+                props.history.push("/");
+                handleSuccessfulLogout();
+            }
+            return response.data;
+        })
+        .catch(error => {
+            console.log("Error signing out", error);
+        });
     };
     
     return (
@@ -38,9 +56,11 @@ const NavigationContainer = (props) => {
 
             <div className="right-side">
                 SHEILA GORDO
+
+                {props.loggedInStatus === "LOGGED_IN" ? (<a onClick={handleSignOut}>Sign Out</a>) : null} 
             </div>
         </div>
     )
 } 
 
-export default NavigationContainer;
+export default withRouter(NavigationContainer);
